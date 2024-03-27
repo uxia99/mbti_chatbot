1eefab9732e308be8a062f38967b25af0ae35a4c
장유정 <306406j@gmail.com>
2024-03-28 00:46:04 +0900

    Edit api/system-prompt/route.ts File

--- src/app/api/system-prompt/route.ts
+++ src/app/api/system-prompt/route.ts
@@ -1,9 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+interface CreateSystemPromptRequest {
+  title: string;
+  prompt: string;
+}
 const prisma = new PrismaClient();
 
 export async function GET() {
   const systemPrompts = await prisma.systemPrompt.findMany();
   return NextResponse.json(systemPrompts);
 }
+
+// export async function POST(req: NextRequest) {
+//   const { title, prompt }: CreateSystemPromptRequest = await req.json();
+
+//   const newSystemPrompt = await prisma.systemPrompt.create({
+//     data: {
+//       title,
+//       prompt,
+//     },
+//   });
+//   return NextResponse.json(newSystemPrompt);
+// }
